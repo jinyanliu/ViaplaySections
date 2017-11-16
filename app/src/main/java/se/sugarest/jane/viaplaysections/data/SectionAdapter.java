79213c8dd3b3dd02465b025961bc82eaa1deed18
jinyanliu <jane.jinyan.liu@gmail.com>
2017-11-16 21:24:58 +0100

    Refactor SectionAdapter java.

--- app/src/main/java/se/sugarest/jane/viaplaysections/data/SectionAdapter.java
+++ app/src/main/java/se/sugarest/jane/viaplaysections/data/SectionAdapter.java
@@ -1,6 +1,5 @@
 package se.sugarest.jane.viaplaysections.data;
 
-import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,22 +11,37 @@ import java.util.ArrayList;
 import se.sugarest.jane.viaplaysections.R;
 
 /**
+ * This class gets a list of section titles from inputs, then populates to the recycler view, which
+ * in fact acts as left drawer navigation view.
+ * <p>
+ * That means the controller in the MVC pattern.
+ * <p>
  * Created by jane on 17-11-14.
  */
-
 public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.SectionAdapterViewHolder> {
-
-    private static final String LOG_TAG = SectionAdapter.class.getSimpleName();
-
+    // An On-click handler to make it easy for MainActivity to interact with the RecyclerView
     private final SectionAdapterOnClickHandler mClickHandler;
     private ArrayList<String> mSectionTitleString = new ArrayList<>();
 
-    private Cursor mCursor;
-
-    public SectionAdapter(SectionAdapterOnClickHandler mClickHandler) {
-        this.mClickHandler = mClickHandler;
+    /**
+     * Creates a SectionAdapter.
+     *
+     * @param clickHandler The on-click handler for this adapter. This single handler is called
+     *                     when an item is clicked.
+     */
+    public SectionAdapter(SectionAdapterOnClickHandler clickHandler) {
+        this.mClickHandler = clickHandler;
     }
 
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param parent   The ViewGroup that these ViewHolders are contained within.
+     * @param viewType If RecyclerView has more than one type of item (which this one don't)
+     *                 this viewType can be used to provide a different layout.
+     * @return A new NoteAdapterViewHolder that holds the View for each list item
+     */
     @Override
     public SectionAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
@@ -35,11 +49,24 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.SectionA
         return new SectionAdapterViewHolder(view);
     }
 
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, update the contents of the ViewHolder to display the movie
+     * posters for each particular position, using the "position" argument that is conveniently
+     * passed in.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the
+     *                 contents of the item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(SectionAdapterViewHolder holder, int position) {
-            holder.mTitleTextView.setText(mSectionTitleString.get(position));
+        holder.mTitleTextView.setText(mSectionTitleString.get(position));
     }
 
+    /**
+     * @return The number of items to display.
+     */
     @Override
     public int getItemCount() {
         if (null != mSectionTitleString) {
@@ -48,36 +75,25 @@ public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.SectionA
         return 0;
     }
 
-    public interface SectionAdapterOnClickHandler {
-        void onClick(String title);
-    }
-
-
-    public void setUpTitleStringArray(ArrayList<String> newArray){
+    /**
+     * This method is used to set the sections' titles string list data on a SectionAdapter
+     */
+    public void setUpTitleStringArray(ArrayList<String> newArray) {
         mSectionTitleString = newArray;
         notifyDataSetChanged();
     }
 
-//    public void swapCursor(Cursor newCursor) {
-//        mCursor = newCursor;
-//        if (null != mCursor) {
-//            for (int i = 0; i < mCursor.getCount(); i++) {
-//                mCursor.moveToPosition(i);
-//                String currentTitle = mCursor.getString(mCursor.getColumnIndex(SectionEntry.COLUMN_SECTION_TITLE));
-//                if (!mSectionTitleString.contains(currentTitle)) {
-//                    mSectionTitleString.add(currentTitle);
-//                }
-//                Log.i(LOG_TAG, "There are " + mSectionTitleString.size() + " different section titles available.");
-//            }
-//        }
-//        notifyDataSetChanged();
-//    }
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface SectionAdapterOnClickHandler {
+        void onClick(String title);
+    }
 
     /**
-     * Cache of the children views for a section title.
+     * Cache of the children views for a section's title.
      */
     public class SectionAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
         public final TextView mTitleTextView;
 
         public SectionAdapterViewHolder(View itemView) {
