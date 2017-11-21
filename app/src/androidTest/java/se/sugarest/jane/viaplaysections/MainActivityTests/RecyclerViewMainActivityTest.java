5775f0900ab76e8e721f833cd984bad4da6b4a3f
jinyanliu <jane.jinyan.liu@gmail.com>
2017-11-21 15:36:03 +0100

    Small changes to RecyclerViewMainActivityTest.

--- app/src/androidTest/java/se/sugarest/jane/viaplaysections/MainActivityTests/RecyclerViewMainActivityTest.java
+++ app/src/androidTest/java/se/sugarest/jane/viaplaysections/MainActivityTests/RecyclerViewMainActivityTest.java
@@ -1,12 +1,9 @@
 package se.sugarest.jane.viaplaysections.MainActivityTests;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -38,15 +35,6 @@ public class RecyclerViewMainActivityTest {
     private SectionAdapter sectionAdapter;
     private ArrayList<String> sectionTitlesStrings = new ArrayList<>();
 
-    private IdlingResource mIdlingResource;
-
-    // Registers any resource that needs to be synchronized with Espresso before the test is run.
-    @Before
-    public void registerIdlingResource() {
-        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
-        Espresso.registerIdlingResources(mIdlingResource);
-    }
-
     @Before
     public void setUp() {
         sectionAdapter = new SectionAdapter(null);
@@ -69,13 +57,5 @@ public class RecyclerViewMainActivityTest {
 
         onView(withId(R.id.title_on_the_app_bar)).check(matches(notNullValue()))
                 .check(matches(withText(secondSection)));
-    }
-
-    // Remember to unregister resources when not needed to avoid malfunction.
-    @After
-    public void unregisterIdlingResource() {
-        if (mIdlingResource != null) {
-            Espresso.unregisterIdlingResources(mIdlingResource);
-        }
     }
 }
